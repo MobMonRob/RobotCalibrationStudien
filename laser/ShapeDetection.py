b2341a1a95c709dd7270da3833a66fe1f99682db
Marcel Reith <Marcel.Reith@googlemail.com>
2022-04-12 21:34:06 +0200

    changed code for new Marker position, moved Code in Functions

--- laser/ShapeDetection.py
+++ laser/ShapeDetection.py
@@ -1,71 +1,62 @@
 # import the necessary packages
 import imutils
 import cv2
+import numpy as np
 
+def GetGrayImage(image):
+    h, w, c = image.shape
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-path = r'D:\Nextcloud\Marcel\DHBW\Studienarbeit\Laser Bilderkennung\TestShape2.png'
+    if cv2.countNonZero(gray) > ((w*h)//2):
+        # More White than Black, so invert the color
+        gray = cv2.bitwise_not(gray)
+    
+    return gray
 
-image = cv2.imread(path)
-h, w, c = image.shape
+def GetCenterOfCountours(image):
+    h, w, c = image.shape
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # find contours in the thresholded image
+    thresh = cv2.threshold(image, 60, 255, cv2.THRESH_BINARY)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
 
-if cv2.countNonZero(gray) > ((w*h)//2):
-    # More White than Black, so invert the color
-    gray = cv2.bitwise_not(gray)
+    centers = []
 
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    # loop over the contours
+    for c in cnts:
+        M = cv2.moments(c)    
+        if not M["m00"] == 0.0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
 
-cv2.imshow("Blured", blurred)
-thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+            #add centerpoints to centers
+            center = [cX, cY]
+            centers.append(center)
 
-# find contours in the thresholded image
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = imutils.grab_contours(cnts)
+            # draw the contour and center of the shape on the image
+            cv2.drawContours(image, [c], -1, (0, 255, 255), 1)
+            cv2.circle(image, (cX, cY), 1, (255, 255, 255), -1)
 
-centers = []
+    if len(centers) > 5:
+        print ("Mehr als 4 Marker erkannt. Kalibrierung kann nicht durchgeführt werden.")
+    else:
+        #check wich point is the left, the right, the top and he bottom one
+        for center in centers:
+            if center[0] < w / 4 and center[1] < h / 4:
+                leftTop = center
+                continue
+            if center[0] > w * 3/4 and center[1] < h / 4:
+                rightTop = center
+                continue
+            if center[0] < w / 4 and center[1] > h * 3/4:
+                leftBottom = center
+                continue
+            if center[0] > w * 3/4 and center[1] > h * 3/4:
+                rightBottom = center                
+                
+        pts = np.array([leftTop, leftBottom, rightBottom, rightTop], np.int32)    
+        ptsReshaped = pts.reshape((-1, 1, 2))
+        image = cv2.polylines(image, [ptsReshaped], True, (255,0,0), 1)
 
-# loop over the contours
-for c in cnts:
-    M = cv2.moments(c)    
-    if not M["m00"] == 0.0:
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-
-        #add centerpoints to centers
-        center = [cX, cY]
-        centers.append(center)
-
-        # draw the contour and center of the shape on the image
-        cv2.drawContours(image, [c], -1, (0, 255, 255), 1)
-        cv2.circle(image, (cX, cY), 1, (255, 255, 255), -1)
-        #cv2.putText(image, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-
-
-if len(centers) > 5:
-    print ("Mehr als 4 Marker erkannt. Kalibrierung kann nicht durchgeführt werden.")
-else:
-    #check wich point is the left, the right, the top and he bottom one
-    for center in centers:
-        if(center[0] < w / 4):
-            left = (center[0], center[1])
-            continue
-        if(center[0] > w * 3/4):
-            right = (center[0], center[1])
-            continue
-        if(center[1] < h / 4):
-            top = (center[0], center[1])
-            continue
-        if(center[1] > h * 3/4):
-            bottom = (center[0], center[1])
-            
-    cv2.line(image, left, right, (255,0,0), 1)
-    cv2.line(image, top, bottom, (0,255,0), 1)
-
-    #show Image
-    cv2.imshow("Image", image)
-
-    while True:
-        if cv2.waitKey(1) == 27: 
-            break  # esc to quit
-    cv2.destroyAllWindows()
+    return pts, image
