2319381480e335ae3f381bbb257ddf1fcc7fc46f
Marcel Reith <Marcel.Reith@googlemail.com>
2022-04-12 21:34:24 +0200

    moved code in function

--- laser/transform.py
+++ laser/transform.py
@@ -1,34 +1,37 @@
 import cv2
 import numpy as np
 import LineDetection as detection
-
-
+from BaslerCamera import Camera
 
 
 def TransformImage(img, pts1, w, h):
     pts2 = np.float32([[0,0],[w,0],[0,h],[w,h]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
-    output = cv2.warpPerspective(img, matrix,(w, h))
+    return cv2.warpPerspective(img, matrix,(w, h))
 
-    for i in range(0,4):
-        cv2.circle(img, (pts1[i][0],pts1[i][1]), 5, (0,255,255),cv2.FILLED)
-    return img, output
 
 if __name__ == "__main__":
-    path = "Test\Test11.png"
+
+    path = "D:\\repos\RobotCalibrationStudien\laser\Test\Test11.png"
 
     img = cv2.imread(path)
+    #camera = Camera()
+    #img = camera.GetImage()
     h, w, c = img.shape
-    w,h = 1000,500
+    #w,h = 1000,500
     
     pts1 = np.float32([[138,131],[644,92],[138,410],[644,463]])
 
-    imgWithCyrcle, output = TransformImage(img, pts1, w, h)
+    output = TransformImage(img, pts1, w, h)
+    
+    for i in range(0,4):
+        cv2.circle(img, (pts1[i][0],pts1[i][1]), 5, (0,255,255),cv2.FILLED)
+    #blue, green, red = cv2.split(img)
+    blured = detection.GetBluredImage(img)
+    #cv2.imshow("img", red)
+    x, y, lines = detection.GetCenterPoint(img, blured)
 
-    blured = detection.GetBluredImage(output)
-    x, y, lines = detection.GetCenterPoint(output, blured)
-
-    cv2.imshow("Original", imgWithCyrcle)
+    cv2.imshow("Original", img)
     cv2.imshow("Transformed", output)
     cv2.imshow("Lines", lines)
     cv2.waitKey(0)
